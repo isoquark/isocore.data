ee135c82ee92495dfe9be37a8ccca896d4466ff9
Chris Moore <isoquark@gmail.com>
2015-07-09 17:07:32 -0500

    Added test and shell projects

---
+++ IQ.Core.MetaCode.Test/TestConfiguration.fs
@@ -0,0 +1,34 @@
+﻿namespace IQ.Core.MetaCode.Test
+
+open System
+open System.Reflection
+
+open IQ.Core.Framework
+open IQ.Core.TestFramework
+open IQ.Core.Data
+open IQ.Core.Data.Sql
+
+
+
+
+[<AutoOpen>]
+module TestConfiguration =
+    
+    //This is instantiated/cleaned-up once per collection
+    type ProjectTestContext()= 
+        inherit TestContext(thisAssembly(), 
+            fun registry -> registry.RegisterFactory(fun config -> config |> SqlDataStore.access)) 
+
+    [<Literal>]
+    let TestCollectionName = "Core Synthetics Tests"
+
+    //This class exists to feed the test infrastructure metadata
+    [<AbstractClass; TestCollectionMaker(TestCollectionName)>]
+    type ProjectCollectionMarker() = 
+        inherit TestCollection<ProjectTestContext>()
+
+    [<AbstractClass; TestContainer(TestCollectionName)>]
+    type ProjectTestContainer(context,log) =
+        member this.Context : ProjectTestContext = context
+        member this.Log : ITestLog = log
+
